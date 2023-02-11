68aebdf2255c7acd9341a28c44f11703d39b1c51
antoniombgondim <125151124+antoniombgondim@users.noreply.github.com>
2023-02-11 13:35:35 -0500

    Update firstpython-branch.py

--- firstpython-branch.py
+++ firstpython-branch.py
@@ -1,1 +1,2 @@
-## in this branch, the main branch is copied, and we can change tit as we wanna here, without altering the main project, by adding files, codes, etc.
+## in this branch, the main branch is copied, and we can change tit as we wanna here, without altering the main project, by adding files, codes, etc. 
+## But, if you want to merge this branch into the main one, you have to do a PULL REQUEST
